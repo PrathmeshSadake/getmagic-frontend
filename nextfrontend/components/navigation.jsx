8645ebdc5f38721bba7bf41ef3a9b669e7c94084
andylnkd <dhalwani1@gmail.com>
2022-09-15 17:22:18 +0000

    Got Link working, got Meta Head tags on key pages working. All prof!

---
+++ nextfrontend/components/navigation.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Navigation(props){
+
+return(
+
+<section className="bg-orange-300 font-medium">
+
+  <Link href={""}> </Link>
+</section>
+
+)
+
+
+}
