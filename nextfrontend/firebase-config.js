2b1dea2d8734301469c2a2133c1c47ba4a97fb40
andylnkd <dhalwani1@gmail.com>
2022-09-16 20:51:47 +0000

    Setup formdata and user reducer

---
+++ nextfrontend/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBrmYoZXiIwroQNhVwInV_X4G_eYYlf4Vk",
+  authDomain: "getmagic-fe.firebaseapp.com",
+  projectId: "getmagic-fe",
+  storageBucket: "getmagic-fe.appspot.com",
+  messagingSenderId: "324734552886",
+  appId: "1:324734552886:web:3fd69a723a20500d6cc197",
+  measurementId: "G-G55CJHDQQS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- nextfrontend/pages/signup.jsx
+++ nextfrontend/pages/signup.jsx
@@ -1,14 +1,38 @@
 import SignUpLayout from "../layouts/signuplayout";
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect, useReducer } from "react";
+import { validateArgCount } from "@firebase/util";
 
+
+const handleAction = (e) => {
+  e.preventDefault();
+  // TODO- set the data for username and call firebase
+
+  console.log(e.target.name)
+  alert("You've just signed up! Get ready for some action!");
+};
+
+const formReducer = (state, event) => {
+return {...state, [event.target.name]: event.target.value}
+}
 export default function SignUp(props) {
+  const [formData, setFormData] = useReducer(formReducer, {}); //passing in a form Reducer function, and an empty initial value
+  
+
   return (
-    <div className="">
-      <div className="flex flex-row items-center justify-center gap-5 h-screen">
-        <input placeholder="User Name" className="flex justify-center align-middle" />
-        <input placeholder="Password" />
-      <button className=" border border-slate-600 bg-blue-200 w-20 font-bold text-orange-400"> Sign Up! </button>
-      </div>
+    <div className="flex flex-row items-center justify-center h-screen">
+      <form onSubmit={handleAction} className="m-2 p-2"> {/*We are using a Form element here with a basic setup and a callback for on click*/}
+        <fieldset className="p-5 m-2 gap-5 border border-black">
+          <input name="name" placeholder="User Name" className="m-2" onChange={setFormData}/>
+          <input name="email" placeholder="Email" className="m-2" onChange={setFormData}/>
+          <input name="password" placeholder="Password" className="m-2" onChange={setFormData}/>
+        </fieldset>
+        <button
+          type="submit"
+          className=" border border-slate-600 bg-blue-200 w-20 font-bold text-orange-400"
+        >
+          Sign Up!
+        </button>
+      </form>
     </div>
   );
 }
