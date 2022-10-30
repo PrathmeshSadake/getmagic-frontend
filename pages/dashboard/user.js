199ebf7056e96e711af6302ee2e2fc9b1a596e51
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-10-30 14:13:31 +0530

    Added multistep form and changed ui

--- pages/dashboard/user.js
+++ pages/dashboard/user.js
@@ -1,20 +1,54 @@
 import React from "react";
-import MultiStep from "react-multistep";
+import { MultiStepForm, Step } from "react-multi-form";
 import Layout from "../../layout/Layout";
 import StepOne from "../../components/multistep-form/StepOne";
 import StepTwo from "../../components/multistep-form/StepTwo";
 
 const User = () => {
-  const steps = [
-    { title: "StepOne", component: <StepOne /> },
-    { title: "StepTwo", component: <StepTwo /> },
-  ];
+  const [active, setActive] = React.useState(1);
   return (
     <Layout>
       <div>
         <h1 className='font-medium text-3xl'>Tell us about your speciality</h1>
-
-        <MultiStep showNavigation={true} steps={steps} />
+        <div className='p-12'>
+          <MultiStepForm activeStep={active}>
+            <Step label='user info'>
+              <StepOne />
+            </Step>
+            <Step label='specialization'>
+              <StepTwo />
+            </Step>
+          </MultiStepForm>
+        </div>
+        <div className='bg-gray-50 px-4 py-3 text-right sm:px-6 lg:mx-12'>
+          {active !== 1 && (
+            <button
+              onClick={() => setActive(active - 1)}
+              style={{ float: "left" }}
+              className='inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700'
+            >
+              Previous
+            </button>
+          )}
+          {active !== 2 && (
+            <button
+              onClick={() => setActive(active + 1)}
+              style={{ float: "right" }}
+              className='inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700'
+            >
+              Next
+            </button>
+          )}
+          {active == 2 && (
+            <button
+              onClick={() => setActive(active + 1)}
+              style={{ float: "right" }}
+              className='inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700'
+            >
+              Save
+            </button>
+          )}
+        </div>
       </div>
     </Layout>
   );
