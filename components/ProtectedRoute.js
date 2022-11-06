2d9118c8ee59597eebf64532b943e3c30dc21103
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-11-06 16:11:02 +0530

    created a Protected Route

--- components/ProtectedRoute.js
+++ components/ProtectedRoute.js
@@ -1,15 +1,24 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase-config";
 
 const ProtectedRoute = ({ children }) => {
   const router = useRouter();
-  const user = { name: "Prathmesh" };
+  const [user, setUser] = useState(null);
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUser(user);
+    } else {
+      setUser(null);
+    }
+  });
 
   useEffect(() => {
     if (!user) {
       router.push("/");
     }
-  });
+  }, [user, router]);
 
   return <>{user ? children : null}</>;
 };
