f21f1df67daaf9baa6a85aee0adcb072f9138cb8
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-11-11 18:44:17 +0530

    Created Auth context

---
+++ pages/creator-landing.js
@@ -0,0 +1,12 @@
+import LandingSignUp from "../components/LandingSignUp";
+import Pricing from "../components/Pricing";
+
+const CreatorLanding = () => {
+  return (
+    <div>
+      <LandingSignUp />
+      <Pricing />
+    </div>
+  );
+};
+export default CreatorLanding;
