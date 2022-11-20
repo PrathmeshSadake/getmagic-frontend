42d39ea9b404dfc2358460406e83b2f889f4d159
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-11-20 13:41:46 +0530

    Added new landing page and GT Walsheim Pro Font

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/router";
 import React from "react";
-import ProtectedRoute from "../components/ProtectedRoute";
 import { auth } from "../config/firebase-config";
 import { AuthProvider } from "../context/AuthContext";
+import "../assets/fonts/GT Walsheim/stylesheet.css";
+import gsap from "gsap";
 import "../styles/globals.css";
 
 const openRoutes = [
@@ -24,6 +25,35 @@ function MyApp({ Component, pageProps }) {
     }
   });
 
+  // Code for cursor animation
+  // https://codepen.io/GreenSock/pen/WNNNBpo
+  if (typeof window !== "undefined") {
+    // Client-side-only code
+    gsap.set(".ball", { xPercent: -50, yPercent: -50 });
+    const ball = document.querySelector(".ball");
+    const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    const mouse = { x: pos.x, y: pos.y };
+    const speed = 0.2;
+
+    const xSet = gsap.quickSetter(ball, "x", "px");
+    const ySet = gsap.quickSetter(ball, "y", "px");
+
+    window.addEventListener("mousemove", (e) => {
+      mouse.x = e.x;
+      mouse.y = e.y;
+    });
+
+    gsap.ticker.add(() => {
+      // adjust speed for higher refresh monitors
+      const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+      pos.x += (mouse.x - pos.x) * dt;
+      pos.y += (mouse.y - pos.y) * dt;
+      xSet(pos.x);
+      ySet(pos.y);
+    });
+  }
+
   return (
     <React.Fragment>
       {openRoutes.includes(router.pathname) ? (
@@ -39,6 +69,8 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </AuthProvider>
       )}
+      {/* Custom Cursor */}
+      <div class='ball hidden lg:block'></div>
     </React.Fragment>
   );
 }
