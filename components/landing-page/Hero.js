8059ea1dde2f3df5b0dfdd916044a7af19877c60
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-12-28 18:28:55 +0530

    modified hero component for only-navbar for privacy policy page

--- components/landing-page/Hero.js
+++ components/landing-page/Hero.js
@@ -14,11 +14,11 @@ const navigation = [
   { name: "Become a Creator", href: "#" },
 ];
 
-export default function Example() {
+export default function Hero({ onlyNav }) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className='min-h-screen isolate bg-black'>
+    <div className={`${!onlyNav && "min-h-screen"} isolate bg-black`}>
       <div className='absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]'>
         <svg
           className='relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]'
@@ -139,9 +139,7 @@ export default function Example() {
                       className='inline-block rounded-lg px-4 py-1.5 text-base font-medium leading-6 bg-gray-700 text-gray-200 shadow-sm'
                       href='/signin'
                     >
-
                       Log in
-
                     </Link>
                   </div>
                 </div>
@@ -150,75 +148,77 @@ export default function Example() {
           </Dialog>
         </div>
       </div>
-      <main>
-        <div className='relative px-6 lg:px-8'>
-          <div className='mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40'>
-            <div>
-              <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
-                <div className='relative overflow-hidden rounded-full py-1.5 px-4 text-base leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
-                  <span className='text-gray-200'>
-                    Connect with Top Brands looking to hire{" "}
-                    <a href='#' className='font-medium text-indigo-600'>
-                      <span className='absolute inset-0' aria-hidden='true' />
-                      Creative Talent Today{" "}
-                      <span aria-hidden='true' className='ml-1'>
+      {!onlyNav && (
+        <main>
+          <div className='relative px-6 lg:px-8'>
+            <div className='mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40'>
+              <div>
+                <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
+                  <div className='relative overflow-hidden rounded-full py-1.5 px-4 text-base leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
+                    <span className='text-gray-200'>
+                      Connect with Top Brands looking to hire{" "}
+                      <a href='#' className='font-medium text-indigo-600'>
+                        <span className='absolute inset-0' aria-hidden='true' />
+                        Creative Talent Today{" "}
+                        <span aria-hidden='true' className='ml-1'>
+                          &rarr;
+                        </span>
+                      </a>
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <h1 className='text-white text-4xl font-medium tracking-tight sm:text-center sm:text-6xl'>
+                    Your Creativity Matters, Follower Counts Don’t Matter
+                  </h1>
+                  <p className='mt-6 text-xl leading-8 text-gray-200 sm:text-center'>
+                    Get Paid with clear built-in contracts in place - Never get
+                    cheated again!
+                  </p>
+                  <div className='mt-8 flex gap-x-4 sm:justify-center'>
+                    <a
+                      href='#'
+                      className='inline-block rounded-md bg-indigo-600 px-4 py-1.5 text-base font-medium leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700'
+                    >
+                      Get started
+                      <span className='text-indigo-200 ml-3' aria-hidden='true'>
                         &rarr;
                       </span>
                     </a>
-                  </span>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <h1 className='text-white text-4xl font-medium tracking-tight sm:text-center sm:text-6xl'>
-                  Your Creativity Matters, Follower Counts Don’t Matter
-                </h1>
-                <p className='mt-6 text-xl leading-8 text-gray-200 sm:text-center'>
-                  Get Paid with clear built-in contracts in place - Never get
-                  cheated again!
-                </p>
-                <div className='mt-8 flex gap-x-4 sm:justify-center'>
-                  <a
-                    href='#'
-                    className='inline-block rounded-md bg-indigo-600 px-4 py-1.5 text-base font-medium leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700'
+                <div className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'>
+                  <svg
+                    className='relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]'
+                    viewBox='0 0 1155 678'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
                   >
-                    Get started
-                    <span className='text-indigo-200 ml-3' aria-hidden='true'>
-                      &rarr;
-                    </span>
-                  </a>
+                    <path
+                      fill='url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)'
+                      fillOpacity='.3'
+                      d='M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z'
+                    />
+                    <defs>
+                      <linearGradient
+                        id='ecb5b0c9-546c-4772-8c71-4d3f06d544bc'
+                        x1='1155.49'
+                        x2='-78.208'
+                        y1='.177'
+                        y2='474.645'
+                        gradientUnits='userSpaceOnUse'
+                      >
+                        <stop stopColor='#4337C9' />
+                        <stop offset={1} stopColor='#4337C9' />
+                      </linearGradient>
+                    </defs>
+                  </svg>
                 </div>
-              </div>
-              <div className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'>
-                <svg
-                  className='relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]'
-                  viewBox='0 0 1155 678'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    fill='url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)'
-                    fillOpacity='.3'
-                    d='M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z'
-                  />
-                  <defs>
-                    <linearGradient
-                      id='ecb5b0c9-546c-4772-8c71-4d3f06d544bc'
-                      x1='1155.49'
-                      x2='-78.208'
-                      y1='.177'
-                      y2='474.645'
-                      gradientUnits='userSpaceOnUse'
-                    >
-                      <stop stopColor='#4337C9' />
-                      <stop offset={1} stopColor='#4337C9' />
-                    </linearGradient>
-                  </defs>
-                </svg>
               </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      )}
     </div>
   );
 }
